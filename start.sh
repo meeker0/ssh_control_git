d20c7d7160ec0e526737a57f7fca23ff756bd6e8
Meek_0 <74826217+meeker0@users.noreply.github.com>
2020-12-15 23:17:31 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
-        sudo apt-get install  qbittorrent-nox &&
-        sudo snap install  ngrok
+        sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable &&
+        sudo apt-get install qbittorrent-nox &&
+        sudo snap install ngrok
 
-        echo  ngrok authotoken
+        echo ngrok authotoken
         read NGROK_TOKEN
         ngrok authtoken $NGROK_TOKEN 
         qbittorrent-nox --webui-port =6565 & ngrok http 6565
